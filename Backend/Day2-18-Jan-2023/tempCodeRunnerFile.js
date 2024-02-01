c6eee8a6ef2e34665140f9c55f78771294a40db6
monarch-009 <adityasinghmoni@gmail.com>
2024-02-01 10:19:38 +0530

    Js

--- Backend/Day2-18-Jan-2023/tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-//write a js program to find the area of a triangle where lengths of the three of its sides are 5, 6, 7.
-// let side1 = 5;
-// let side2 = 6;
-// let side3 = 7;
-// let s = (side1 + side2 + side3) / 2;
-// let area = Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
-// console.log("Area of triangle: " + area.toFixed(2)); // toFixed() is a method of Number() object
