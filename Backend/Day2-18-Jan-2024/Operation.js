385d140fe23ae13ee12379a66a0ee8958cbb91dd
monarch-009 <adityasinghmoni@gmail.com>
2024-02-01 10:19:06 +0530

    Js

---
+++ Backend/Day2-18-Jan-2024/Operation.js
@@ -0,0 +1,87 @@
+// Objective: Basic operations in JavaScript
+
+// Challenge 1 : Perform the addition operation (add x and y) and print the result in the console.
+let x = 5;
+let y = 2;
+let add = x + y;
+console.log("Add: " + add);
+
+// Challenge 2 : Perform the subtraction operation (subtract x and y) and print the result in the console.
+let sub = x - y;
+console.log("Subtract: " + sub);
+
+// Challenge 3 : Perform the multiplication operation (multiply x and y) and print the result in the console.
+let mul = x * y;
+console.log("Multiply: " + mul);
+
+// Challenge 4 : Perform the division operation (divide x and y) and print the result in the console.
+let div = x / y;
+console.log("Divide: " + div);
+
+// Challenge 5 : Perform the remainder operation (x % y) and print the result in the console.
+let rem = x % y;
+console.log("Reminder: " + rem);
+
+// Challenge 6 : Perform the exponential operation (x ** y) and print the result in the console.
+let exp = x ** y;
+console.log("Exponent: "+ exp);
+
+// Challenge 7 : Calculate "x" to the power of "y" and print the result in the console.
+let pow = Math.pow(x, y);
+console.log("Power: " + pow);
+
+// Challenge 8 : Decrement the value of "x" by 1 and print the result in the console.
+x--;
+console.log("Decrement: " + x);
+
+// Challenge 9 : Perform Addition assignemnt operation to the variable a;
+let a = 10;
+a += 5;
+console.log("Addition Assignment: " + a);
+
+// Challenge 10 : Perform Subtraction assignemnt operation to the variable b;
+let b = 10;
+b -= 5;
+console.log("Subtraction Assignment: " + b);
+
+// Challenge 11 : Perform Multiplication assignemnt operation to the variable c;
+let c = 10;
+c *= 5;
+console.log("Multiplication Assignment: " + c);
+
+// Challenge 12 : Perform Division assignemnt operation to the variable d;
+let d = 10;
+d /= 5;
+console.log("Division Assignment: " + d);
+
+// Challenge 13 : Perfrom 2 Strict Equality operations and print the result in the console.
+let e = 10;
+let f = 10;
+console.log("Strict Equality: " + (e === f));
+
+// Challenge 14 : Perfrom 2 Strict non-equality operations and print the result in the console.
+let g = 10;
+let h = 10;
+console.log("Strict Non-Equality: " + (g !== h));
+
+// Challenge 15 : Perfrom 2 Equality operations and print the result in the console.
+let i = 10;
+let j = 10;
+console.log("Equality: " + (i == j));
+
+// Challenge 16 : Perfrom 2 non-equality operations and print the result in the console.
+let k = 10;
+let l = 10;
+console.log("Non-Equality: " + (k != l));
+
+
+// Challenge 17 : Perfrom 2 Less than or equal to operations in which one should return true and another should return false.
+let m = 10;
+let n = 20;
+console.log("Less than or equal to: " + (m <= n));
+
+// Challenge 18 : Perfrom 2 Greater than or equal to operations in which one should return true and another should return false.
+let o = 10;
+let p = 20;
+console.log("Greater than or equal to: " + (o >= p));
+
