74fa260b1d832a75b21eaf92b1a32e851b2ca0e7
monarch-009 <adityasinghmoni@gmail.com>
2024-02-16 10:22:20 +0530

    Basic of html & js

---
+++ Frontend/Day5-9-Feb-2024/script.js
@@ -0,0 +1,37 @@
+// // Iterate through an object 
+
+// const obj = {
+//     name: "Aditya ",
+//     age: 21,
+//     mobile: 1234567890,
+//     email: "adi@gmail.com",
+//     address: "India"
+// };
+
+// for (let key in obj) {
+//     console.log(`${key}: ${obj[key]}`);
+// }
+// // Update the value of a key in an object
+// obj.name = "Aditya Singh Rajput";
+// console.log(obj);
+
+// // Using a for in loop, print all the even number upto 100 in line 
+
+// for (let i = 0; i <= 100; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+
+// // Write a function that calculate the greatest common divisor of two numbers, Take two numbers as parameters and returns the greatest common divisor.
+
+// function gcd(a, b) {
+//     if (b === 0) {
+//         return a;
+//     }
+//     return gcd(b, a % b);
+// }
+// console.log(gcd(12, 8));
+
+
+// //
