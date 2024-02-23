363e31ab4bc4fd756b8327691f8d06c850066a8b
monarch-009 <adityasinghmoni@gmail.com>
2024-02-23 10:26:02 +0530

    Key function work

---
+++ Frontend/Day7-16-Feb-2024/script.js
@@ -0,0 +1,28 @@
+// let input = document.getElementById('input');
+// let output = document.getElementById('output');
+
+// input.addEventListener('keydown', function(event) {
+//     output.innerHTML = `Key: ${event.key}, Code: ${event.code}`;
+// });
+
+// // onkeyup
+// input.addEventListener('keyup', function(event) {   
+//     output.innerHTML = `Key: ${event.key}, Code: ${event.code}`;
+// }
+// );
+// // onkeypress
+// input.addEventListener('keypress', function(event) {
+//     output.innerHTML = `Key: ${event.key}, Code: ${event.code}`;
+// });
+
+function datetime(){
+    document.getElementById("demo").innerHTML = new Date();
+    document.getElementById("demo").style.fontSize = "70px";
+    document.getElementById("demo").style.color = "cadetblue";
+    document.getElementById("demo").style.fontStyle = "italic";
+    document.getElementById("demo").style.cssText = "font-size: 70px; color: brown";
+}
+
+function stop(){
+    clea
+}
